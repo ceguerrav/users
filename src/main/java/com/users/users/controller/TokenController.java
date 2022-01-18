6a5aa19ceb4c9a2810a9447a705ddf90d43283a4
ceguerrav <ce.guerrav@gmail.com>
2022-01-17 22:17:45 -0300

    control de excpciones restantes

--- src/main/java/com/users/users/controller/TokenController.java
+++ src/main/java/com/users/users/controller/TokenController.java
@@ -7,6 +7,7 @@ import com.users.users.dto.UserDTO;
 import com.users.users.exception.UserException;
 import com.users.users.service.UserService;
 import com.users.users.utils.ConstantUtil;
+import com.users.users.utils.ResponseUtil;
 import com.users.users.utils.TokenUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,31 +31,38 @@ public class TokenController {
 
     private final UserService userService;
 
-
-
     @PostMapping("/refresh")
     public ResponseEntity<Object> getToken(@RequestBody TokenDTO tokenDTO) throws UserException {
-        ResponseDTO response = ResponseDTO.builder().build();
-        UserDTO user = userService.getUserBy(tokenDTO.getEmail(), tokenDTO.getPassword());
-        if (Objects.nonNull(user)) {
-            String token = TokenUtil.signToken(tokenDTO.getEmail());
-            response.setMessage(ConstantUtil.TOKEN_MESSAGE + token);
-        }else {
-            response.setMessage(ConstantUtil.USER_PASS_NO_VALID);
-            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        try {
+            ResponseDTO response = ResponseDTO.builder().build();
+            UserDTO user = userService.getUserBy(tokenDTO.getEmail(), tokenDTO.getPassword());
+            if (Objects.nonNull(user)) {
+                String token = TokenUtil.signToken(tokenDTO.getEmail());
+                response.setMessage(ConstantUtil.TOKEN_MESSAGE + token);
+            } else {
+                response.setMessage(ConstantUtil.USER_PASS_NO_VALID);
+                return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+            }
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception ex) {
+            return ResponseUtil.buildResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/verify")
-    public ResponseEntity<ResponseDTO> getToken(@RequestHeader(value = "token") String token) {
-        ResponseDTO response = ResponseDTO.builder()
-            .message(ConstantUtil.TOKEN_VALID)
-            .build();
-        if(!TokenUtil.verifyToken(token)) {
-            response.setMessage(ConstantUtil.NO_VALID_TOKEN);
-            return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<Object> getToken(@RequestHeader(value = "token") String token) {
+        try {
+            ResponseDTO response = ResponseDTO.builder()
+                .message(ConstantUtil.TOKEN_VALID)
+                .build();
+
+            if(!TokenUtil.verifyToken(token)) {
+                response.setMessage(ConstantUtil.NO_VALID_TOKEN);
+                return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+            }
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception ex) {
+            return ResponseUtil.buildResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-     return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
